48394751d69912b39dda5ee6ab4325e168d031e9
hasibul islam <hasibulislam.web@gmail.com>
2019-10-28 23:46:36 -0700

    created cat-section, featured section and services section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 (function ($) {
 "use strict";
 
+// owl-carousel
+$('.slider-active').owlCarousel({
+    loop:true,
+    nav: true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        }
+    }
+})
+
 
 
 })(jQuery);
