f37baa08e9baac15d93fc00fe237b93d0fed9af1
hasibul islam <hasibulislam.web@gmail.com>
2019-10-31 23:08:42 -0700

    created testimonial-area, footer-area

--- js/main.js
+++ js/main.js
@@ -18,6 +18,84 @@ $('.slider-active').owlCarousel({
         }
     }
 })
+// project-active
+$('.project-active').owlCarousel({
+    loop:true,
+    dots: false,
+    autoplay: true,
+    nav: true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 3
+        },
+        1000:{
+            items: 4
+        }
+    }
+})
+
+// Blog active
+$('.blog-active').owlCarousel({
+    loop:true,
+    autoplay: true,
+    nav: true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        }
+    }
+})
+
+// testimonial active
+$('.testimonial-active').owlCarousel({
+    loop:true,
+    nav: true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        }
+    }
+})
+// brand-active
+$('.brand-active').owlCarousel({
+    loop:true,
+    autoplay: true,
+    nav: false,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 4
+        },
+        1000:{
+            items: 6
+        }
+    }
+})
+
+$('.popup-video').magnificPopup({
+    type: 'iframe'
+})
 
 
 
